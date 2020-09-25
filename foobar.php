e1312955803de7922d16ecc6fd388d22fe34a234
Dharmesh Chaudhary <chaudharydharmesh32@gmail.com>
2020-09-25 14:08:59 +1000

    Code formating

--- foobar.php
+++ foobar.php
@@ -8,18 +8,18 @@
 
 	for($counter = 1; $counter <= 100; $counter++) {
 		switch($counter) {
-            case ($counter%3 == 0 && $counter%5 == 0):
-            echo "foobar<br/>";
-            break;
-            case ($counter%3 == 0):
-            echo "foo<br/>";
-            break;
-            case ($counter%5 ==0):
-            echo "bar<br/>";
-            break;
-            default:
-            echo $counter."<br/>";
-            break;
-        }
+				case ($counter%3 == 0 && $counter%5 == 0):
+				echo "foobar<br/>";
+				break;
+				case ($counter%3 == 0):
+				echo "foo<br/>";
+				break;
+				case ($counter%5 ==0):
+				echo "bar<br/>";
+				break;
+				default:
+				echo $counter."<br/>";
+				break;
+			}
 	}
 ?>
