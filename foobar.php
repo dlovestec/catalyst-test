91daf9bccea33d80c21a296573e36b7edfaa089f
Dharmesh Chaudhary <chaudharydharmesh32@gmail.com>
2020-09-25 14:16:34 +1000

    Organize code to prepare desired output and basic commenting.

--- foobar.php
+++ foobar.php
@@ -3,23 +3,28 @@
 	/*
 		Author	: 	Dharmesh Chaudhary
 		E-mail	: 	chaudharydharmesh32@gmail.com
-		Purpose	:	Logic Test 
+		Purpose	:	Logic Test
+		Output	:	1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14, foobar …
 	*/
 
+	/* for loop method */
+	
 	for($counter = 1; $counter <= 100; $counter++) {
 		switch($counter) {
 			case ($counter%3 == 0 && $counter%5 == 0):
-				echo "foobar<br/>";
+				echo "foobar,";
 				break;
 			case ($counter%3 == 0):
-				echo "foo<br/>";
+				echo "foo,";
 				break;
 			case ($counter%5 ==0):
-				echo "bar<br/>";
+				echo "bar,";
 				break;
 			default:
-				echo $counter."<br/>";
+				echo $counter.",";
 				break;
 		}
 	}
+	
+	/* for loop method */
 ?>
