9f82950e9b11e540040dec45087b9941285f47e2
Dharmesh Chaudhary <chaudharydharmesh32@gmail.com>
2020-09-26 18:44:47 +1000

    Created and added main class dlovescatalystclass.php

---
+++ dlovescatalystclass.php
@@ -0,0 +1,143 @@
+<?php
+	class DlovesCatalyst
+	{
+		private $host;
+		private $username;
+		private $password;
+		private $database = "catalyst_test";
+		public $connection;
+		
+		public function __construct()
+		{
+			$args = func_get_args();
+		
+			$this->host = $args[0]['host'];
+			$this->username = $args[0]['username'];
+			$this->password = $args[0]['password'];
+			
+			$this->connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);
+			
+			if (mysqli_connect_errno())
+			{
+				echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				exit;
+			}
+
+			return $this->connection;
+		}
+		
+		public function checkEmail($email) {
+			$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+		public function dropTable()
+		{
+			$droptable = "DROP TABLE IF EXISTS `users`";
+			$this->connection->query($droptable);
+		}
+		
+		public function createTable()
+		{
+			$this->dropTable();
+			
+			$createtable = " CREATE TABLE `users`(`name` VARCHAR(255) NOT NULL,`surname` VARCHAR(255) NOT NULL, `email` VARCHAR(255) NOT NULL) ENGINE = MyISAM AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8;";
+				
+			if ($this->connection->query($createtable) === TRUE) {
+				echo "Table Users created successfully";
+			} else {
+				echo "Error creating table: " . $this->connection->error;
+			}
+		}
+		
+		public function dryRun($filepath)
+		{
+			$file = fopen($filepath, "r");
+			$row = 1;
+			$validrecords = array();
+			$invalidrecords = array();
+			
+			while (($column = fgetcsv($file, 10000, ",")) !== FALSE)
+			{
+				if($row == 1) {
+					$row++;
+					continue;
+				}
+				
+				$name = "";
+				if (isset($column[0])) {
+					$name = $column[0];
+				}
+				
+				$surname = "";
+				if (isset($column[1])) {
+					$surname = $column[1];
+				}
+				
+				$email = "";
+				if (isset($column[2])) {
+					$email = $column[2];
+				}
+			
+				if($this->checkEmail($email) !== FALSE) {
+					$validrecords[$row]['name'] = $column[0];
+					$validrecords[$row]['surname'] = $column[1];
+					$validrecords[$row]['email'] = $column[2];
+				} else {
+					$invalidrecords[$row]['name'] = $column[0];
+					$invalidrecords[$row]['surname'] = $column[1];
+					$invalidrecords[$row]['email'] = $column[2];
+				}
+				$row++;
+			}
+			
+			echo "<pre>";
+			print_r($validrecords);
+			print_r($invalidrecords);
+		}
+		
+		public function parseCSV($path)
+		{
+			$file = fopen($path, "r");
+			$row = 1;
+			$insert = "";
+			
+			while (($column = fgetcsv($file, 10000, ",")) !== FALSE)
+			{
+				if($row == 1) {
+					$row++;
+					continue;
+				}
+				
+				$name = "";
+				if (isset($column[0])) {
+					$name = mysqli_real_escape_string($this->connection, $column[0]);
+				}
+				
+				$surname = "";
+				if (isset($column[1])) {
+					$surname = mysqli_real_escape_string($this->connection, $column[1]);
+				}
+				
+				$email = "";
+				if (isset($column[2])) {
+					$email = mysqli_real_escape_string($this->connection, $column[2]);
+				}
+			
+				if($this->checkEmail($email) !== FALSE) {
+					$insert.="insert into users (`name`,`surname`,`email`) values ('".ucfirst(strtolower($name))."','".ucfirst(strtolower($surname))."','".strtolower($email)."');";
+				}
+			}
+			
+			if (mysqli_multi_query($this->connection, $insert)) {
+				echo "New records created successfully";
+			} else {
+				echo "Error: " . $insert . "<br>" . mysqli_error($this->connection);
+			}
+		}
+	}
+?>
